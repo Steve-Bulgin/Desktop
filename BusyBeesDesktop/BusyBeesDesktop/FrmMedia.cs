373a876729ea549af3cc54c9d132cc364e846b35
Steve Bulgin <steven.bulgin@gmail.com>
2017-04-19 13:58:38 -0400

    Need clear pbs, dynamic paths

--- BusyBeesDesktop/BusyBeesDesktop/FrmMedia.cs
+++ BusyBeesDesktop/BusyBeesDesktop/FrmMedia.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,6 +17,8 @@ namespace BusyBeesDesktop
     {
         FrmMain frmMain;
         List<PictureBox> pictureBoxList = new List<PictureBox>();
+        string hivefromlv;
+        string hiveid;
         SQLiteConnection conn = new SQLiteConnection("Data Source=" + @"G:\Visual Studio 2015\Projects\BeeApiaryData\apiary.db");
 
         static int brh = 0;
@@ -54,94 +57,88 @@ namespace BusyBeesDesktop
 
         private void lstHiveMedia_SelectedIndexChanged(object sender, EventArgs e)
         {
-            MessageBox.Show(lstHiveMedia.GetItemText(lstHiveMedia.SelectedItem));
+            
+            hivefromlv = lstHiveMedia.GetItemText(lstHiveMedia.SelectedItem);
+            hiveid = Regex.Match(hivefromlv, @"((?<=Hive )\d+)").ToString();
+
+            string[] folders = Directory.GetDirectories(@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images");
+
+            foreach (string folder in folders)
+            {
+                if (folder == @"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\" + hiveid)
+                {
+                    string[] images = Directory.GetFiles(@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\" + hiveid);
+
+                    int local = 5;
+                    int counter = 5;
+
+                    foreach (string image in images)
+                    {
+                        string imgname = Regex.Match(image, @"(\d+)[_](\d+)(?=.jpg)").ToString();
+                        if (image != @"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\" + hiveid + @"\Thumbs.db")
+                        {
+
+
+                            counter++;
+                            if ((counter * 100) > 1150)
+                            {
+                                local = local + 100;
+                                counter = 0;
+                            }
+
+                            PictureBox picture = new PictureBox
+                            {
+                                Name = "pictureBox" + imgname,
+                                Size = new Size(100, 100),
+                                Location = new Point(counter * 100, local),
+                                BorderStyle = BorderStyle.FixedSingle,
+                                SizeMode = PictureBoxSizeMode.StretchImage
+                            };
+
+                            picture.ImageLocation = (@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\" + hiveid + @"\") + imgname + ".jpg";
+                            picture.Click += new EventHandler(pictureBox1_Click);
+                            pictureBoxList.Add(picture);
+
+
+
+
+                            foreach (PictureBox p in pictureBoxList)
+                            {
+                                this.Controls.Add(p);
+                            }
+
+                            MessageBox.Show(imgname);
+                            Console.WriteLine(image);
+                            //Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly().Location);
+
+                            
+                        }
+                    }
+
+                }
+            }
         }
 
 
-        //int fileCount = Directory.GetFiles(@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\1").Length;
+        
 
+      
 
-        //int local = 10;
-        //int counter=10;
-        //for (int i = 0; i < fileCount; i++)
-        //{
-        //    counter++;
-        //    if ((counter*100) > 1150)
-        //    {
-        //        local = local +100;
-        //        counter = 0;
-        //    }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+            FrmPicture frmPicture = new FrmPicture(picture.ImageLocation);
+            frmPicture.Show();
 
-        //    PictureBox picture = new PictureBox
-        //    {
-        //        Name = "pictureBox" + i,
-        //        Size = new Size(100, 100),
-        //        Location = new Point(counter * 100, local),
-        //        BorderStyle = BorderStyle.FixedSingle,
-        //        SizeMode = PictureBoxSizeMode.StretchImage
-        //    };
+        }
 
-        //    picture.ImageLocation = (@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\1\") + i + ".jpg";
-        //    picture.Click += new EventHandler(pictureBox1_Click);
-        //    pictureBoxList.Add(picture);
-        //}
-
-        //foreach (PictureBox p in pictureBoxList)
-        //{
-        //    this.Controls.Add(p);
-        //}
-    }
-
-    //private void pictureBox1_Click(object sender, EventArgs e)
-    //{
-    //    PictureBox picture = (PictureBox)sender;
-    //    FrmPicture frmPicture = new FrmPicture(picture.ImageLocation);
-    //    frmPicture.Show();
-
-    //}
-
-
-    //private void FrmMedia_FormClosing(object sender, FormClosingEventArgs e)
-    //{
-    //    frmMain.Show();
-    //}
-
-    //private void lstHiveMedia_SelectedIndexChanged(object sender, EventArgs e)
-    //{
-    //    // Get hive Information into the text boxes
-    //    try
-    //    {
-    //        conn.Open();
-    //        String sql = "SELECT * FROM Hive Where hiveID =" + (listHive.SelectedIndex + 1);
-    //        SQLiteCommand cmd3 = new SQLiteCommand(sql, conn);
-    //        SQLiteDataReader reader = cmd3.ExecuteReader();
-
-    //        while (reader.Read())
-    //        {
-    //            lblHive.Text = reader.GetString(1);
-    //            //  lblSplit.Text = reader.GetString(2).ToString();
-    //            lblType.Text = reader.GetString(3);
-    //            lblSource.Text = reader.GetDouble(4).ToString();
-    //            lblConfig.Text = reader.GetString(5);
-    //            lblYard.Text = reader.GetDouble(6).ToString();
-    //        }
-
-    //        conn.Close();
-    //    }
-    //    catch (Exception m)
-    //    {
-
-    //        MessageBox.Show("An Error Occured" + m.Message);
-    //    }
-
-
-
-
-
-
-
+        private void FrmMedia_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            frmMain.Show();
+        }
 
     }
+}
 
 
 
