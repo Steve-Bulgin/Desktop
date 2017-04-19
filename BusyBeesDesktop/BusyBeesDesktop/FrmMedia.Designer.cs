69b1d6311497149e899e547bec38ad2da23ba113
Steve Bulgin <steven.bulgin@gmail.com>
2017-04-19 10:47:14 -0400

    Starting to figure it out

--- BusyBeesDesktop/BusyBeesDesktop/FrmMedia.Designer.cs
+++ BusyBeesDesktop/BusyBeesDesktop/FrmMedia.Designer.cs
@@ -28,21 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstHiveMedia = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstHiveMedia
+            // 
+            this.lstHiveMedia.FormattingEnabled = true;
+            this.lstHiveMedia.Location = new System.Drawing.Point(24, 51);
+            this.lstHiveMedia.Name = "lstHiveMedia";
+            this.lstHiveMedia.Size = new System.Drawing.Size(215, 381);
+            this.lstHiveMedia.TabIndex = 0;
+            this.lstHiveMedia.SelectedIndexChanged += new System.EventHandler(this.lstHiveMedia_SelectedIndexChanged);
             // 
             // FrmMedia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1199, 627);
+            this.Controls.Add(this.lstHiveMedia);
             this.Name = "FrmMedia";
             this.Text = "FrmMedia";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMedia_FormClosing);
             this.Load += new System.EventHandler(this.FrmMedia_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstHiveMedia;
     }
 }
--- BusyBeesDesktop/BusyBeesDesktop/FrmMedia.cs
+++ BusyBeesDesktop/BusyBeesDesktop/FrmMedia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -15,6 +16,7 @@ namespace BusyBeesDesktop
     {
         FrmMain frmMain;
         List<PictureBox> pictureBoxList = new List<PictureBox>();
+        SQLiteConnection conn = new SQLiteConnection("Data Source=" + @"G:\Visual Studio 2015\Projects\BeeApiaryData\apiary.db");
 
         static int brh = 0;
         public FrmMedia(FrmMain frmMain)
@@ -26,55 +28,122 @@ namespace BusyBeesDesktop
     
         private void FrmMedia_Load(object sender, EventArgs e)
         {
-            int fileCount = Directory.GetFiles(Path.GetPathRoot(System.Reflection.Assembly.GetEntryAssembly().Location) + @"BusyBees\Images").Length;
-           
-         
-            int local = 1;
-            int counter=-1;
-            for (int i = 0; i < fileCount; i++)
+
+            //fill the list boxes with hives
+            try
             {
-                counter++;
-                if ((counter*100) > 1150)
+                conn.Open();
+                String sql = "SELECT * FROM Hive";
+                SQLiteCommand cmd3 = new SQLiteCommand(sql, conn);
+                SQLiteDataReader reader = cmd3.ExecuteReader();
+
+                while (reader.Read())
                 {
-                    local = local +100;
-                    counter = 0;
+                    lstHiveMedia.Items.Add("Hive " + reader.GetDouble(0));
                 }
-
-                PictureBox picture = new PictureBox
-                {
-                    Name = "pictureBox" + i,
-                    Size = new Size(100, 100),
-                    Location = new Point(counter * 100, local),
-                    BorderStyle = BorderStyle.FixedSingle,
-                   SizeMode = PictureBoxSizeMode.StretchImage
-                };
-                
-                picture.ImageLocation = Path.GetPathRoot(System.Reflection.Assembly.GetEntryAssembly().Location) + @"BusyBees\Images\" + i + ".png";
-                picture.Click += new EventHandler(pictureBox1_Click);
-                pictureBoxList.Add(picture);
             }
-            
-            foreach (PictureBox p in pictureBoxList)
+            catch (Exception m)
             {
-                this.Controls.Add(p);
+
+                MessageBox.Show("Database Not found! Please import from the tablet!");
             }
+
+            conn.Close();
+
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void lstHiveMedia_SelectedIndexChanged(object sender, EventArgs e)
         {
-            PictureBox picture = (PictureBox)sender;
-            FrmPicture frmPicture = new FrmPicture(picture.ImageLocation);
-            frmPicture.Show();
-
-        }
-     
-
-        private void FrmMedia_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            frmMain.Show();
+            MessageBox.Show(lstHiveMedia.GetItemText(lstHiveMedia.SelectedItem));
         }
 
-       
+
+        //int fileCount = Directory.GetFiles(@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\1").Length;
+
+
+        //int local = 10;
+        //int counter=10;
+        //for (int i = 0; i < fileCount; i++)
+        //{
+        //    counter++;
+        //    if ((counter*100) > 1150)
+        //    {
+        //        local = local +100;
+        //        counter = 0;
+        //    }
+
+        //    PictureBox picture = new PictureBox
+        //    {
+        //        Name = "pictureBox" + i,
+        //        Size = new Size(100, 100),
+        //        Location = new Point(counter * 100, local),
+        //        BorderStyle = BorderStyle.FixedSingle,
+        //        SizeMode = PictureBoxSizeMode.StretchImage
+        //    };
+
+        //    picture.ImageLocation = (@"G:\Visual Studio 2015\Projects\BeeApiaryData\Images\1\") + i + ".jpg";
+        //    picture.Click += new EventHandler(pictureBox1_Click);
+        //    pictureBoxList.Add(picture);
+        //}
+
+        //foreach (PictureBox p in pictureBoxList)
+        //{
+        //    this.Controls.Add(p);
+        //}
+    }
+
+    //private void pictureBox1_Click(object sender, EventArgs e)
+    //{
+    //    PictureBox picture = (PictureBox)sender;
+    //    FrmPicture frmPicture = new FrmPicture(picture.ImageLocation);
+    //    frmPicture.Show();
+
+    //}
+
+
+    //private void FrmMedia_FormClosing(object sender, FormClosingEventArgs e)
+    //{
+    //    frmMain.Show();
+    //}
+
+    //private void lstHiveMedia_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    // Get hive Information into the text boxes
+    //    try
+    //    {
+    //        conn.Open();
+    //        String sql = "SELECT * FROM Hive Where hiveID =" + (listHive.SelectedIndex + 1);
+    //        SQLiteCommand cmd3 = new SQLiteCommand(sql, conn);
+    //        SQLiteDataReader reader = cmd3.ExecuteReader();
+
+    //        while (reader.Read())
+    //        {
+    //            lblHive.Text = reader.GetString(1);
+    //            //  lblSplit.Text = reader.GetString(2).ToString();
+    //            lblType.Text = reader.GetString(3);
+    //            lblSource.Text = reader.GetDouble(4).ToString();
+    //            lblConfig.Text = reader.GetString(5);
+    //            lblYard.Text = reader.GetDouble(6).ToString();
+    //        }
+
+    //        conn.Close();
+    //    }
+    //    catch (Exception m)
+    //    {
+
+    //        MessageBox.Show("An Error Occured" + m.Message);
+    //    }
+
+
+
+
+
+
+
 
     }
-}
+
+
+
+
+
